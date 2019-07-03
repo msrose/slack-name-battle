e78e838a2d76acba4996b9a70461ebbdb6eab591
Michael Rose <michael_rose@outlook.com>
2019-07-02 23:11:36 -0400

    Use util to promisify dynamodb methods

--- name-battle/dynamodb.js
+++ name-battle/dynamodb.js
@@ -1,4 +1,5 @@
 const aws = require('aws-sdk')
+const util = require('util')
 
 const dynamodbOptions = {
     region: process.env.AWS_REGION || 'us-east-1',
@@ -10,47 +11,36 @@ if (process.env.AWS_SAM_LOCAL) {
 
 const dynamodb = new aws.DynamoDB.DocumentClient(dynamodbOptions)
 
+const query = util.promisify(dynamodb.query.bind(dynamodb))
+const put = util.promisify(dynamodb.put.bind(dynamodb))
+
+const TableName = process.env.NAME_BATTLE_TABLE_NAME
+
 function getDocumentsBySlackId(id) {
-    return new Promise((res, rej) => {
-        const now = Math.floor(Date.now() / 1000)
-        dynamodb.query(
-            {
-                TableName: process.env.NAME_BATTLE_TABLE_NAME,
-                KeyConditions: {
-                    slack_id: {
-                        ComparisonOperator: 'EQ',
-                        AttributeValueList: [id],
-                    },
-                    timestamp: {
-                        ComparisonOperator: 'GT',
-                        AttributeValueList: [now],
-                    },
-                },
+    const now = Math.floor(Date.now() / 1000)
+    return query({
+        TableName,
+        KeyConditions: {
+            slack_id: {
+                ComparisonOperator: 'EQ',
+                AttributeValueList: [id],
             },
-            (err, data) => {
-                if (err) return rej(err)
-                res(data)
+            timestamp: {
+                ComparisonOperator: 'GT',
+                AttributeValueList: [now],
             },
-        )
+        },
     })
 }
 
 function putDocumentBySlackId(id, value, timestamp) {
-    return new Promise((res, rej) => {
-        dynamodb.put(
-            {
-                TableName: process.env.NAME_BATTLE_TABLE_NAME,
-                Item: {
-                    slack_id: id,
-                    timestamp,
-                    lifeForce: value,
-                },
-            },
-            (err, data) => {
-                if (err) return rej(err)
-                res(data)
-            },
-        )
+    return put({
+        TableName,
+        Item: {
+            slack_id: id,
+            timestamp,
+            lifeForce: value,
+        },
     })
 }
 
