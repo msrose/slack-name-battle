00d5f029d15dc7981dc6545d903b9d297bda605b
Michael Rose <michael_rose@outlook.com>
2019-07-11 23:56:42 -0400

    More refactoring

--- name-battle/app.js
+++ name-battle/app.js
@@ -52,6 +52,188 @@ function getResponse(
     return lines.join('\n')
 }
 
+async function handleCommand(tokens, attackerUserId) {
+    const command = tokens[0]
+
+    if (!command || command === 'help') {
+        return {
+            statusCode: 200,
+            body: JSON.stringify({
+                text:
+                    'Usage: `/name-battle @<target> | status | stats | help | leaders`',
+            }),
+        }
+    }
+
+    if (command === 'leaders') {
+        const { leaders, total } = await getLeaderboard()
+        const leadersWithNames = await Promise.all(
+            leaders.map(({ slackId, ratio }) =>
+                getRealName(slackId).then(name => ({ name, ratio })),
+            ),
+        )
+        return {
+            statusCode: 200,
+            body: JSON.stringify({
+                response_type: 'in_channel',
+                text: `*Leaderboard* (_Top ${
+                    leaders.length
+                } of ${total}_)\n${leadersWithNames
+                    .map(
+                        ({ name, ratio }, i) =>
+                            `${i + 1}. *${name}*: ${ratio.toFixed(3)}`,
+                    )
+                    .join('\n')}`,
+            }),
+        }
+    }
+
+    if (command === 'stats') {
+        const [realName, stats] = await Promise.all([
+            getRealName(attackerUserId),
+            getStats(attackerUserId),
+        ])
+        const getStat = stat => stats[stat] || 0
+        return {
+            statusCode: 200,
+            body: JSON.stringify({
+                response_type: 'in_channel',
+                text: [
+                    `Stats for *${realName}*:`,
+                    [
+                        `:trophy: Victories: *${getStat('kills')}*`,
+                        `:skull: Defeats: *${getStat('deaths')}*`,
+                        `:garbage_fire: Suicides: *${getStat('suicides')}*`,
+                    ].join(' | '),
+                ].join('\n'),
+            }),
+        }
+    }
+
+    if (command === 'status') {
+        const [attackerDebuffs, attackerUsedManna] = await Promise.all([
+            getTotalDebuffs(attackerUserId),
+            getUsedManna(attackerUserId),
+        ])
+
+        const getStatusBar = (max, value, length, unit, empty) => {
+            const percentage = Math.max(max - value, 0) / max
+            return Array(length)
+                .fill()
+                .map((_, i) => (i < length * percentage ? unit : empty))
+                .join('')
+        }
+        const healthBar = getStatusBar(
+            100,
+            attackerDebuffs,
+            20,
+            ':heart:',
+            ':black_heart:',
+        )
+        const mannaBar = getStatusBar(
+            5,
+            attackerUsedManna,
+            20,
+            ':collision:',
+            ':anger:',
+        )
+        const health = Math.max(100 - attackerDebuffs, 0) / 100
+        const fixedHealth = (health * 100).toFixed(2)
+        const manna = Math.max(5 - attackerUsedManna, 0) / 5
+        const fixedManna = (manna * 100).toFixed(2)
+        const attackerName = await getRealName(attackerUserId)
+        return {
+            statusCode: 200,
+            body: JSON.stringify({
+                text: [
+                    `Status for *${attackerName}*:`,
+                    `\`${'Life Force'.padStart(
+                        10,
+                    )}\`: [${healthBar}] ${fixedHealth}%`,
+                    `\`${'Attacks'.padStart(
+                        10,
+                    )}\`: [${mannaBar}] ${fixedManna}%`,
+                ].join('\n'),
+            }),
+        }
+    }
+}
+
+async function conductNameBattle(attackerUserId, targetUserId) {
+    const [attackerDebuffs, attackerUsedManna] = await Promise.all([
+        getTotalDebuffs(attackerUserId),
+        getUsedManna(attackerUserId),
+    ])
+
+    if (attackerDebuffs >= 100) {
+        return {
+            statusCode: 200,
+            body: JSON.stringify({
+                text: "You can't Name Battle: you're dead!",
+            }),
+        }
+    }
+
+    if (attackerUsedManna >= 5) {
+        return {
+            statusCode: 200,
+            body: JSON.stringify({
+                text: "You can't Name Battle: out of attacks!",
+            }),
+        }
+    }
+
+    const [attacker, target, targetDebuffs] = await Promise.all([
+        getRealName(attackerUserId),
+        getRealName(targetUserId),
+        getTotalDebuffs(targetUserId),
+    ])
+
+    let newLifeForce
+    const didAttackerChangeName = await didNameChange(attackerUserId, attacker)
+
+    if (didAttackerChangeName) {
+        await putDebuff(attackerUserId, 100, 15)
+    } else {
+        const lifeForce = nameBattle({ attacker, target })
+
+        newLifeForce = lifeForce - targetDebuffs
+
+        const isKill = newLifeForce <= 0 && newLifeForce + (100 - lifeForce) > 0
+
+        if (newLifeForce > 0 || isKill) {
+            // TODO: put these in a transaction
+            await Promise.all([
+                putDebuff(targetUserId, 100 - lifeForce),
+                useManna(attackerUserId),
+                recordBattle(attackerUserId, targetUserId, isKill),
+            ])
+        } else {
+            return {
+                statusCode: 200,
+                body: JSON.stringify({
+                    text: "You can't Name Battle: target is already dead!",
+                }),
+            }
+        }
+    }
+
+    return {
+        statusCode: 200,
+        body: JSON.stringify({
+            response_type: 'in_channel',
+            text: getResponse(
+                attackerUserId,
+                targetUserId,
+                attacker,
+                target,
+                newLifeForce,
+                didAttackerChangeName,
+            ),
+        }),
+    }
+}
+
 exports.lambdaHandler = async event => {
     let response
 
@@ -70,187 +252,17 @@ exports.lambdaHandler = async event => {
         const parameters = new URLSearchParams(event.body)
 
         const tokens = parameters.get('text').split(' ')
-
-        const command = tokens[0]
-
-        if (!command || command === 'help') {
-            return {
-                statusCode: 200,
-                body: JSON.stringify({
-                    text:
-                        'Usage: `/name-battle @<target> | status | stats | help | leaders`',
-                }),
-            }
-        }
-
-        if (command === 'leaders') {
-            const { leaders, total } = await getLeaderboard()
-            const leadersWithNames = await Promise.all(
-                leaders.map(({ slackId, ratio }) =>
-                    getRealName(slackId).then(name => ({ name, ratio })),
-                ),
-            )
-            return {
-                statusCode: 200,
-                body: JSON.stringify({
-                    response_type: 'in_channel',
-                    text: `*Leaderboard* (_Top ${
-                        leaders.length
-                    } of ${total}_)\n${leadersWithNames
-                        .map(
-                            ({ name, ratio }, i) =>
-                                `${i + 1}. *${name}*: ${ratio.toFixed(3)}`,
-                        )
-                        .join('\n')}`,
-                }),
-            }
-        }
-
         const attackerUserId = parameters.get('user_id')
 
-        if (command === 'stats') {
-            const [realName, stats] = await Promise.all([
-                getRealName(attackerUserId),
-                getStats(attackerUserId),
-            ])
-            const getStat = stat => stats[stat] || 0
-            return {
-                statusCode: 200,
-                body: JSON.stringify({
-                    response_type: 'in_channel',
-                    text: [
-                        `Stats for *${realName}*:`,
-                        [
-                            `:trophy: Victories: *${getStat('kills')}*`,
-                            `:skull: Defeats: *${getStat('deaths')}*`,
-                            `:garbage_fire: Suicides: *${getStat('suicides')}*`,
-                        ].join(' | '),
-                    ].join('\n'),
-                }),
-            }
+        const result = await handleCommand(tokens, attackerUserId)
+
+        if (result) {
+            return result
         }
 
-        const [attackerDebuffs, attackerUsedManna] = await Promise.all([
-            getTotalDebuffs(attackerUserId),
-            getUsedManna(attackerUserId),
-        ])
+        const targetUserId = tokens[0].split('|')[0].replace(/[<@]/g, '')
 
-        if (command === 'status') {
-            const getStatusBar = (max, value, length, unit, empty) => {
-                const percentage = Math.max(max - value, 0) / max
-                return Array(length)
-                    .fill()
-                    .map((_, i) => (i < length * percentage ? unit : empty))
-                    .join('')
-            }
-            const healthBar = getStatusBar(
-                100,
-                attackerDebuffs,
-                20,
-                ':heart:',
-                ':black_heart:',
-            )
-            const mannaBar = getStatusBar(
-                5,
-                attackerUsedManna,
-                20,
-                ':collision:',
-                ':anger:',
-            )
-            const health = Math.max(100 - attackerDebuffs, 0) / 100
-            const fixedHealth = (health * 100).toFixed(2)
-            const manna = Math.max(5 - attackerUsedManna, 0) / 5
-            const fixedManna = (manna * 100).toFixed(2)
-            const attackerName = await getRealName(attackerUserId)
-            return {
-                statusCode: 200,
-                body: JSON.stringify({
-                    text: [
-                        `Status for *${attackerName}*:`,
-                        `\`${'Life Force'.padStart(
-                            10,
-                        )}\`: [${healthBar}] ${fixedHealth}%`,
-                        `\`${'Attacks'.padStart(
-                            10,
-                        )}\`: [${mannaBar}] ${fixedManna}%`,
-                    ].join('\n'),
-                }),
-            }
-        }
-
-        if (attackerDebuffs >= 100) {
-            return {
-                statusCode: 200,
-                body: JSON.stringify({
-                    text: "You can't Name Battle: you're dead!",
-                }),
-            }
-        }
-
-        if (attackerUsedManna >= 5) {
-            return {
-                statusCode: 200,
-                body: JSON.stringify({
-                    text: "You can't Name Battle: out of attacks!",
-                }),
-            }
-        }
-
-        const targetUserId = command.split('|')[0].replace(/[<@]/g, '')
-
-        const [attacker, target, targetDebuffs] = await Promise.all([
-            getRealName(attackerUserId),
-            getRealName(targetUserId),
-            getTotalDebuffs(targetUserId),
-        ])
-
-        let newLifeForce
-        const didAttackerChangeName = await didNameChange(
-            attackerUserId,
-            attacker,
-        )
-
-        if (didAttackerChangeName) {
-            await putDebuff(attackerUserId, 100, 15)
-        } else {
-            const lifeForce = nameBattle({ attacker, target })
-
-            newLifeForce = lifeForce - targetDebuffs
-
-            const isKill =
-                newLifeForce <= 0 && newLifeForce + (100 - lifeForce) > 0
-
-            if (newLifeForce > 0 || isKill) {
-                // TODO: put these in a transaction
-                await Promise.all([
-                    putDebuff(targetUserId, 100 - lifeForce),
-                    useManna(attackerUserId),
-                    recordBattle(attackerUserId, targetUserId, isKill),
-                ])
-            } else {
-                return {
-                    statusCode: 200,
-                    body: JSON.stringify({
-                        text: "You can't Name Battle: target is already dead!",
-                    }),
-                }
-            }
-        }
-
-        response = {
-            statusCode: 200,
-            body: JSON.stringify({
-                response_type: 'in_channel',
-                text: getResponse(
-                    attackerUserId,
-                    targetUserId,
-                    attacker,
-                    target,
-                    newLifeForce,
-                    didAttackerChangeName,
-                ),
-            }),
-        }
+        response = await conductNameBattle(attackerUserId, targetUserId)
     } catch (err) {
         logError(err)
 
